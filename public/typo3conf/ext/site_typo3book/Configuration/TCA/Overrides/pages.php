7b58eedcb833b3ba2f976c33d1eeca58712a2ed1
Elli Ludwigson <elli@openstrategypartners.com>
2018-10-16 16:15:12 +0200

    Added PageTSConfig etc to set user access on all pages

---
+++ public/typo3conf/ext/site_typo3book/Configuration/TCA/Overrides/pages.php
@@ -0,0 +1,7 @@
+<?php
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
+    'site_typo3book',
+    'Configuration/PageTSConfig/PageTSConfig.txt',
+    'PageTSConfiguration for typo3book'
+);
