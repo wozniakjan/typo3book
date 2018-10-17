8899395a9b471004e519a1472171f4af1ee84061
Elli Ludwigson <elli@openstrategypartners.com>
2018-10-17 09:57:31 +0200

    Organized tsconfig files into PageTs, added templates

--- public/typo3conf/ext/site_typo3book/Configuration/TCA/Overrides/pages.php
+++ public/typo3conf/ext/site_typo3book/Configuration/TCA/Overrides/pages.php
@@ -2,6 +2,6 @@
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
     'site_typo3book',
-    'Configuration/PageTSConfig/PageTSConfig.txt',
+    'Configuration/PageTs/main.tsconfig',
     'PageTSConfiguration for typo3book'
 );
